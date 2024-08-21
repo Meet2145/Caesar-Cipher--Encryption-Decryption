ca0730ae350d0ef40673c6af5bf11477d36dfbf9
Meet Patel <meetpatel2145@gmail.com>
2024-08-21 08:48:56 -0500

    Add files via upload

---
+++ Encrypting the text.py
@@ -0,0 +1,22 @@
+import string 
+
+def caesar_encrypt(message, key):
+    shift = key % 26
+    cipher = str.maketrans(string.ascii_lowercase, string.ascii_lowercase[shift:] + string.ascii_lowercase[:shift])
+    encrypted_message = message.lower().translate(cipher)
+    return encrypted_message 
+
+def caesar_decrypt(encrypted_message, key):
+    shift = key % 26
+    cipher = str.maketrans(string.ascii_lowercase, string.ascii_lowercase[-shift:] + string.ascii_lowercase[:-shift])
+    message = encrypted_message.translate(cipher)
+    return message
+
+message = input("Type the text you want to Encrypt: ")
+key = 3 
+
+encrypted_message = caesar_encrypt(message, key)
+print(f"Encrypted message: {encrypted_message}")
+
+decrypted_message = caesar_decrypt(encrypted_message, key)
+print(f"Decrypted message: {decrypted_message}")
